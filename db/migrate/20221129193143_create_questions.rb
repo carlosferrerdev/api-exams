9c47fb0a807a987bd4a806c393f8a43a42c00491
Carlos Ferrer <carlosvferrer@gmail.com>
2022-11-29 16:31:55 -0300

    Question model created

---
+++ db/migrate/20221129193143_create_questions.rb
@@ -0,0 +1,10 @@
+class CreateQuestions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :questions do |t|
+      t.text :statement
+      t.references :exam, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
